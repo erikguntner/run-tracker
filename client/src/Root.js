7da56fd4684dca8bfc66cf0d0468ba5decae73ef
Erik <eguntner2291@gmail.com>
2019-01-18 17:40:02 -0800

    added function to fix redux devtools error in prod

--- client/src/Root.js
+++ client/src/Root.js
@@ -2,20 +2,26 @@ import React from "react";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import reducers from "./reducers";
 import watcherSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
-const reduxDevTools =
+let reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
+  reduxDevTools = a => a;
+}
 
 // We can now use this provider tag to power our application, but also wrap our render testing components as well.
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
+    compose(
+      applyMiddleware(sagaMiddleware),
+      reduxDevTools
+    )
   );
 
   sagaMiddleware.run(watcherSaga);
