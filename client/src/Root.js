1f0bb0ed5ae28e7c958c7bc15aec8a411d8079c8
Erik <eguntner2291@gmail.com>
2019-01-18 17:31:13 -0800

    changed redux devtools compose

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducers from './reducers';
-import watcherSaga from './sagas';
+import React from "react";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import reducers from "./reducers";
+import watcherSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // We can now use this provider tag to power our application, but also wrap our render testing components as well.
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, compose(applyMiddleware(sagaMiddleware), reduxDevTools))
+  const store = createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
 
   sagaMiddleware.run(watcherSaga);
 
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  )
-}
+  return <Provider store={store}>{children}</Provider>;
+};
