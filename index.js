a061db5ae1d4018d69e2551e090829257b2b9a96
Erik <eguntner2291@gmail.com>
2019-04-18 17:33:23 -0700

    updated production app

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ router(app);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+  app.use('*', express.static('client/build')); // Added this
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
